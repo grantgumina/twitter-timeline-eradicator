b0250187f16c9b9beffaa742e0bb6d47199ef9a2
Grant Gumina <grantgumina@gmail.com>
2017-10-18 16:21:42 -0700

    smoothing things out

--- src/js/content.js
+++ src/js/content.js
@@ -1,16 +1,15 @@
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            callback(xmlHttp.responseText);
-        } else {
-            callback(null);
-        }
-    }
+var globalQuoteHTML = "";
 
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+function makeHTTPRequest(theUrl)
+{
+    return new Promise(function (resolve, reject) {
+        
+        var request = new XMLHttpRequest();
+        request.onload = resolve;
+        request.onerror = reject;
+        request.open("GET", theUrl, true); // true for asynchronous 
+        request.send(null);
+    });
 }
 
 function quoteBuilder(author, quote, link) {
@@ -18,26 +17,55 @@ function quoteBuilder(author, quote, link) {
     return quoteHTML;
 }
 
-var timelineElement = document.querySelector('div#timeline');
-var dashboardElement = document.querySelector('div.dashboard-right');
-
-dashboardElement.parentElement.removeChild(dashboardElement);
-timelineElement.innerHTML = "";
-
-httpGetAsync("https://quotes.rest/qod.json", function(jsonString) {
-
-    if (jsonString == null) {
-        timelineElement.innerHTML = quoteBuilder("By Me", "This is an inspirational quote", "");
-        return;
-    }
-
-    var json = JSON.parse(jsonString);
+function parseJSONToGetQuote(json) {
     var author = json['contents']['quotes'][0]['author'];
     var quote = json['contents']['quotes'][0]['quote'];
     var link = json['contents']['quotes'][0]['permalink'];
 
     var inspirationalQuoteString = quoteBuilder(author, quote, link);
 
-    timelineElement.innerHTML = inspirationalQuoteString;
+    return inspirationalQuoteString;
+}
+
+// Find elements on screen
+var timelineElement = document.querySelector('div#timeline');
+var dashboardElement = document.querySelector('div.dashboard-right');
+
+// Clear out the timeline
+if (dashboardElement) 
+    dashboardElement.innerHTML = "";
+
+if (timelineElement)
+timelineElement.innerHTML = "";
+
+// Start listening to the page for changes
+document.addEventListener('DOMSubtreeModified', function () {
+    // if (timelineElement.innerHTML != globalQuoteHTML)
+    //     timelineElement.innerHTML = globalQuoteHTML;
 });
 
+// Set default quote
+globalQuoteHTML = quoteBuilder("By Me", "This is an inspirational quote", "");
+timelineElement.innerHTML = globalQuoteHTML;
+
+// Attempt to get quote of the day
+var httpPromise = Promise.resolve(makeHTTPRequest("https://quotes.rest/qod.json"));
+
+httpPromise.then(function (response) {
+    var json = JSON.parse(response['target']['response']);
+    
+    // Check response json to see if we're being rate limited
+    if (json['error']) {
+        return;
+    }
+    
+    var quoteHTML = parseJSONToGetQuote(json);
+
+    globalQuoteHTML = quoteHTML;
+    timelineElement.innerHTML = quoteHTML;
+
+}, function (error) {
+    console.log(error);
+}).then(function () {
+    
+});
